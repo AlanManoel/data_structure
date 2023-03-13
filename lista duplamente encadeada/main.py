2580762931f07eb6f7c27c8546629dfd571bf555
Alan Manoel <alanmanoel517@gmail.com>
2023-03-13 16:58:32 -0300

    Atualiando as main

--- lista duplamente encadeada/main.py
+++ lista duplamente encadeada/main.py
@@ -1,5 +1,23 @@
 from Doubly_linked_list import *
 
 lista = Doubly_linked_list()
+lista.append(2)
+lista.append(3)
+lista.append(4)
+lista.append(6)
+lista.append(7)
+lista.append(8)
+lista.append(9)
+
+lista.insert(0,1)
+lista.insert(4,5)
+lista.insert(100, 10)
+lista.pop()
+
+lista.remove(9)
+
+print(lista)
 
 print(len(lista))
+
+print(lista.index(7))
