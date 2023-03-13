0172a19d48d1181e9d34834a0be44acf61e200fa
Alan Manoel <alanmanoel517@gmail.com>
2023-03-13 15:42:46 -0300

    Adicionando a função appende

--- lista duplamente encadeada/Doubly_linked_list.py
+++ lista duplamente encadeada/Doubly_linked_list.py
@@ -5,6 +5,34 @@ class Doubly_linked_list:
         self.list = None
         self.size = 0
 
+    def append(self, value):
+        if (self.list == None):
+            self.list = Node(value)
+            self.size += 1
+        else:
+            pointer = self.list
+            while(pointer.right):
+                pointer = pointer.right
+            node = Node(value)
+            node.left = pointer
+            pointer.right = node
+            self.size += 1
+
+    def __repr__(self):
+        if (self.size > 0):
+            pointer = self.list
+            r = ""
+            while(pointer):
+                r += str(pointer.value) + " "
+                pointer = pointer.right
+            return r
+        else:
+            return " "
+        
+    def __str__(self):
+        return self.__repr__()
+
+
     def __len__(self):
         '''Função para retorna o tamanho da lista'''
         return self.size
