0a8533a940da7c37f4e38b56d8ebd4264b93d9e2
Alan Manoel <alanmanoel517@gmail.com>
2023-03-15 16:34:26 -0300

    Atualizando os __pycache__

--- fila/main.py
+++ fila/main.py
@@ -1,6 +1,11 @@
-import Queue
+from Queue  import *
 
+a = Queue()
+for cont in range(1,6):
+    a.add(cont)
 
-
-a = Queue.Queue
+for cont in range(3):
+    a.remove()
+    
 print(a)
+print(len(a))
