a14c90bb5bed18ec4a5626e6d475d3dd32ddd2ff
Alan Manoel <alanmanoel517@gmail.com>
2023-03-20 16:47:26 -0300

    modificando o main da lista encadeada

--- lista encadeada/main.py
+++ lista encadeada/main.py
@@ -1,12 +1,23 @@
 from lista import *
 
-a = lista() 
-a.append(1)
-a.append(2)
-a.append(3)
-a.append(4)
-a.append(3)
+list = lista() #CRIAÇÃO DA LISTA.
 
-print(a[8])
+for cont in range(1,6):
+    list.append(cont) #ADICIOANDO OS VALORES DO 1 AO 5 NA LISTA.
 
-print(len(a))
+print(list) #OUTPUT: 1 2 3 4 5
+print(len(list)) #OUTPUT 5
+
+list.pop() #FUNÇÃO QUE REMOVE O ULTIMO ELEMENTO
+print(list) #OUTPUT: 1 2 3 4 
+print(len(list)) #OUTPUT 4
+
+list.remove(3) #FUNÇÃO DE RETORNAR ELEMENTO PELO VALOR
+print(list) #OUTPUT: 1 2 4 
+print(len(list)) #OUTPUT 3
+
+list.insert(2,3) 
+print(list) #OUTPUT 1 2 3 4
+print(len(list)) #OUTPUT 4
+
+print(list[1]) #OUTPUT 2
